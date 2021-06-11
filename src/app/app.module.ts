092625fdc968e27105a528d3124cfa6484d3a18c
talshbt <53658521+talshbt@users.noreply.github.com>
2021-06-11 14:02:00 +0300

    first-commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductsComponent } from './products/products.component';
+import { AddProductComponent } from './products/add-product/add-product.component';
+import { ProductsListComponent } from './products/products-list/products-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ProductsComponent,
+    AddProductComponent,
+    ProductsListComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
