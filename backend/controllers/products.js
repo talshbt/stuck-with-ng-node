cc2d1ec7db9376d52681174907f38f9dd8112788
talshbt <53658521+talshbt@users.noreply.github.com>
2021-06-14 12:07:08 +0300

    get product by id mongoose

--- backend/controllers/products.js
+++ backend/controllers/products.js
@@ -5,8 +5,6 @@ const router = express.Router();
 //get all products
 router.get("", (req, res, next) => {
   Product.find().then((documents) => {
-    console.log(documents)
-
     res.status(200).json({
       message: "Posts fetched successfully!",
       products: documents,
@@ -30,24 +28,22 @@ router.post("", (req, res, next) => {
 
 //delete product by id
 router.delete("/:id", (req, res, next) => {
-  // prodInd = mockProducts.indexOf(
-  //   mockProducts.filter((prod) => prod.id !== req.params.id)
-  // );
-  // mockProducts.splice(prodInd, 1);
-// console.log(req.params.id)
-  Product.deleteOne({_id: req.params.id}).then((result) => {
+  Product.deleteOne({ _id: req.params.id }).then((result) => {
     console.log(result);
     res.status(200).json({ message: "Post deleted!" });
   });
 });
 
+//get selected product
 router.get("/:id", (req, res, next) => {
-  product = mockProducts.find((prod) => prod.id == req.params.id);
-  res.status(201).json({
-    message: "Post successfully",
-    product: product,
+  Product.findById(req.params.id).then((product) => {
+    res.status(201).json({
+      message: "Post successfully",
+      product: product,
+    });
   });
 });
+
 router.put("/:id", (req, res, next) => {
   prodInd = mockProducts.indexOf(
     mockProducts.find((prod) => prod.id == req.params.id)
