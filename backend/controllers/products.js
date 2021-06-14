4427a9a01048f3e32ad9ca4aaac6b667c7674df2
talshbt <53658521+talshbt@users.noreply.github.com>
2021-06-14 11:51:29 +0300

    add new product to mongo

--- backend/controllers/products.js
+++ backend/controllers/products.js
@@ -1,49 +1,9 @@
 const express = require("express");
-
-const product = require("../models/product");
+const Product = require("../models/product");
 
 const router = express.Router();
 var id = 2;
-// router.post("", (req, res, next) => {
-//   const post = new Post({
-//     title: req.body.title,
-//     content: req.body.content
-//   });
-//   post.save().then(createdPost => {
-//     res.status(201).json({
-//       message: "Post added successfully",
-//       postId: createdPost._id
-//     });
-//   });
-// });
 
-// router.put("/:id", (req, res, next) => {
-//   const post = new Post({
-//     _id: req.body.id,
-//     title: req.body.title,
-//     content: req.body.content
-//   });
-//   Post.updateOne({ _id: req.params.id }, post).then(result => {
-//     res.status(200).json({ message: "Update successful!" });
-//   });
-// });
-
-// router.get("/:id", (req, res, next) => {
-//   Post.findById(req.params.id).then(post => {
-//     if (post) {
-//       res.status(200).json(post);
-//     } else {
-//       res.status(404).json({ message: "Post not found!" });
-//     }
-//   });
-// });
-
-// router.delete("/:id", (req, res, next) => {
-//   Post.deleteOne({ _id: req.params.id }).then(result => {
-//     console.log(result);
-//     res.status(200).json({ message: "Post deleted!" });
-//   });
-// });
 var mockProducts = [
   { title: "tal", content: "flight", id: 0 },
   { title: "eran", content: "flight", id: 1 },
@@ -57,13 +17,15 @@ router.get("", (req, res, next) => {
 });
 
 router.post("", (req, res, next) => {
-  console.log(req.body);
-  product = req.body;
-  product["productId"] = id++;
-  mockProducts.push(product);
-  res.status(201).json({
-    message: "Post added successfully",
-    productId: id++,
+  const product = new Product({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  product.save().then((createdPost) => {
+    res.status(201).json({
+      message: "Post added successfully",
+      productId: product._id,
+    });
   });
 });
 
@@ -89,7 +51,6 @@ router.put("/:id", (req, res, next) => {
   console.log(prodInd);
   mockProducts[prodInd] = req.body;
   res.status(200).json({ message: "Update successful!" });
-
 });
 
 module.exports = router;
